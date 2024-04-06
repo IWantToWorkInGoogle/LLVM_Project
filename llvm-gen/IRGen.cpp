993cff89ee3af3e8e299bfd5050670ae12e03935
Муфтиев Руслан <98661902+IWantToWorkInGoogle@users.noreply.github.com>
2024-04-06 12:26:55 +0300

    Add files via upload

--- llvm-gen/IRGen.cpp
+++ llvm-gen/IRGen.cpp
@@ -118,16 +118,61 @@ int main() {
 
     {
         BasicBlock *BB0 = BasicBlock::Create(context, "0", appFunc);
+        BasicBlock *BB1 = BasicBlock::Create(context, "0", appFunc);
         BasicBlock *BB3 = BasicBlock::Create(context, "3", appFunc);
+        BasicBlock *BB4 = BasicBlock::Create(context, "4", appFunc);
+        BasicBlock *BB11 = BasicBlock::Create(context, "11", appFunc);
+        BasicBlock *BB17 = BasicBlock::Create(context, "17", appFunc);
 
-
-        builder.SetInsertPoint(BB0);
-
-        builder.CreateBr(BB3);
 
         builder.SetInsertPoint(BB3);
 
+
+//    define dso_local void @app() local_unnamed_addr #0 {
+        builder.SetInsertPoint(BB0);
+//        br label %1
+        builder.CreateBr(BB1);
+//
+        builder.SetInsertPoint(BB1);
+//        1:                                                ; preds = %0, %131
+        PHINode *val2 = builder.CreatePHI(builder.getInt32Ty(), 1);
+        val2->addIncoming(builder.getInt32(0), BB0);
+//            %2 = phi i32 [ 0, %0 ], [ %132, %131 ]
+        builder.CreateBr(BB4);
+//            br label %4
+//
+        builder.SetInsertPoint(BB3);
+//        3:                                                ; preds = %131
         builder.CreateRetVoid();
+//            ret void
+//
+        builder.SetInsertPoint(BB4);
+//        4:                                                ; preds = %1, %49
+        PHINode *val5 = builder.CreatePHI(builder.getInt64Ty(), 1);
+        val5->addIncoming(builder.getInt64(0), BB1);
+//            %5 = phi i64 [ 0, %1 ], [ %9, %49 ]
+        Value *val6= builder.CreateICmpEQ(val5, builder.getInt64(0));
+//            %6 = icmp eq i64 %5, 0
+        Value *val7 = builder.CreateAdd(val5, builder.getInt64(4294967295), "", true);
+//            %7 = add nuw i64 %5, 4294967295
+        Value *val8 = builder.CreateAnd(val7, builder.getInt64(4294967295));
+//            %8 = and i64 %7, 4294967295
+        Value *val9 = builder.CreateAdd(val5, builder.getInt64(1), "", true, true);
+//            %9 = add nuw nsw i64 %5, 1
+        Value *val10 = builder.CreateICmpUGT(val5, builder.getInt64(510));
+//            %10 = icmp ugt i64 %5, 510
+        builder.CreateCondBr(val6, BB17, BB11);
+//            br i1 %6, label %17, label %11
+//
+        builder.SetInsertPoint(BB11);
+//        11:                                               ; preds = %4
+//            %12 = getelementptr inbounds [512 x [256 x i8]], [512 x [256 x i8]]* @state, i64 0, i64 %8, i64 0
+//            %13 = load i8, i8* %12, align 16, !tbaa !5, !range !9
+//            %14 = getelementptr inbounds [512 x [256 x i8]], [512 x [256 x i8]]* @state, i64 0, i64 %8, i64 1
+//            %15 = load i8, i8* %14, align 1, !tbaa !5, !range !9
+//            %16 = add nuw nsw i8 %13, %15
+//            br label %17
+        builder.CreateBr(BB17);
     }
 
     builder.SetInsertPoint(entryBB);
